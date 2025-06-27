c1ee29cc6742b5f45e7dae588810d38e63030d10
NaMlex <alexn.alexn@gmail.com>
2025-06-27 15:51:10 +0200

    handling events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefukGreeting';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="I'm a stateful class component" name="Lerzos"/>
+      <MethodsAsPropsParent />
     </div>
   );
 }
---
+++ my-app/src/components/ES7Snipets.js
@@ -0,0 +1,59 @@
+//-- rfce
+
+/* import React from 'react'
+
+function ES7Snipets() {
+  return (
+    <div>ES7Snipets</div>
+  )
+}
+
+export default ES7Snipets */
+
+//-- rafce
+
+/* import React from 'react'
+
+const ES7Snipets = () => {
+  return (
+    <div>ES7Snipets</div>
+  )
+}
+
+export default ES7Snipets */
+
+//-- rce
+
+/* import React, { Component } from 'react'
+
+export class ES7Snipets extends Component {
+  render() {
+    return (
+      <div>ES7Snipets</div>
+    )
+  }
+}
+
+export default ES7Snipets */
+
+//-- rconst
+
+/* constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+} */
+
+//-- clg
+
+/* console.log(first) */
+
+//-- clo
+
+/* console.log('first', first) */
+
+
+
+
